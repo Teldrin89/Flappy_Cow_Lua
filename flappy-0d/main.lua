336e4946b30bc1e37478d0e2167d0c95dc5e666f
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-04-09 23:17:53 +0200

    added push library for virtual resolution game window

--- flappy-0d/main.lua
+++ flappy-0d/main.lua
@@ -1,1 +1,28 @@
--- intial comment for flappy cow game
+--[[
+    second game develped in lua with GD50 course using love2d framework
+    this time the game will be using the external sprites (instead of the
+    single shapes drawn in lua)
+]] 
+
+-- New take on the FLAPPY BIRD game
+
+--                      Game 0 updated - load first sprites to the game
+
+-- setup the game window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+--[[
+    the function to be used to load image files from disk is graphics.newImage(path) that will then load a selected image, place it using the "x" and "y" coordinates and allow to use it as an object
+]]
+
+--[[ 
+    use the external push library to apply the virtual resolution allowing for
+    more retro look of the game
+]]
+
+-- add push library
+push = require 'push'
+-- setup the virtual resolution
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
