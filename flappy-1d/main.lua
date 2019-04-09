a765c44019e4fb62603881f3dc8784a4f8860dc2
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-04-10 00:11:36 +0200

    added the images scrolling

--- flappy-1d/main.lua
+++ flappy-1d/main.lua
@@ -24,7 +24,19 @@ VIRTUAL_HEIGHT = 288
 
 -- define localy used variables for the background and ground images
 local background = love.graphics.newImage('background_night.png')
+-- define the variable to track the scroll of images
+local backgroundScroll = 0
 local ground = love.graphics.newImage('ground_night.png')
+local groundScroll = 0
+--[[ define the values of speed for both ground and background - one shall be       faster than the other to create the ilussion of moving
+    also to avoid ending of each of the images both of them will be looped with
+    looping points
+]]
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+-- set a looping point for both images
+local BACKGROUND_LOOPING_POINT = 413
+local GROUND_LOOPING_POINT = 449
 
 -- load function - runs 1st in running of the game
 function love.load()
@@ -53,14 +65,26 @@ function love.keypressed(key)
     end
 end
 
+-- love update function
+function love.update(dt)
+    -- apply the speed of image scrolling with dt
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        % BACKGROUND_LOOPING_POINT
+    --[[ 
+        "%" defines the modulo operation that will prevent from sudden cuts in 
+        image scrolling
+    ]]
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        % GROUND_LOOPING_POINT
+end
 -- love render function
 function love.draw()
     -- start the push rendering
     push:start()
-
-    love.graphics.draw(background, 0, 0)
+    -- update the draw function with scroll position that will be updated
+    love.graphics.draw(background, backgroundScroll, 0)
     -- draw ground image at the bottom - the size of the ground image
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 32)
+    love.graphics.draw(ground, groundScroll, VIRTUAL_HEIGHT - 32)
     -- end push rendering
     push:finish()
 end
