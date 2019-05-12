6e7ec18477ca06c42784951b97f46adce5412517
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-05-12 22:20:27 +0200

    updated pipe class to work with new pair of pipes class

--- flappy-6d/Pipe.lua
+++ flappy-6d/Pipe.lua
@@ -7,25 +7,42 @@ Pipe = Class{}
     the screen - hence do it before init function
 ]] 
 local PIPE_IMAGE = love.graphics.newImage('pipe_yellow.png')
--- add a value for pipe scrolling - arbitrary - with "-" to shift left
-local PIPE_SCROLL = -60
--- init function for Pipe class - initializes the object
-function Pipe:init()
+-- add a value for pipe scrolling - arbitrary, updated sign
+local PIPE_SCROLL = 60
+-- set the global values of pipe height and width
+PIPE_HEIGHT = 288
+PIPE_WIDTH = 70
+--[[
+    init function for Pipe class - initializes the object, now takes 2
+    parameters: the orientation (top, bottom) and "y" value
+]]
+function Pipe:init(orientation, y)
     -- set the pipe over the right edge
     self.x = VIRTUAL_WIDTH
-    -- set the pipe at random heights of the bottom side
-    self.y = math.random(VIRTUAL_HEIGHT/3.35, VIRTUAL_HEIGHT - 65)
+    -- set the pipe at "y" height (passed parameter)
+    self.y = y
     -- set width as a width of a pipe
     self.width = PIPE_IMAGE:getWidth()
+    -- set height as the pre-defined value
+    self.height = PIPE_HEIGHT
+    -- set the orientation of the pipe based on passed parameter
+    self.orientation = orientation
 end
 
--- set up the update function for pipe
+-- update function has been covered in pair of pipes class
 function Pipe:update(dt)
-    -- setup the update of pipe position by adding the scrolling speed*dt
-    self.x = self.x + PIPE_SCROLL*dt
+
 end
 
 -- set up the rendering of pipe object
 function Pipe:render()
-    love.graphics.draw(PIPE_IMAGE, self.x, self.y)
+    --[[
+        the draw function is now used with additional parameters that handle
+        the rotation (not used) and scale (that will allow to mirror the pipe)
+    ]]
+    love.graphics.draw(PIPE_IMAGE, self.x, 
+        (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y), 
+        0, -- rotation parameter 
+        1, -- X scale (mirror)
+        self.orientation == 'top' and -1 or 1) -- Y scale
 end
